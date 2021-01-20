8f33a4b2cb5af199f6b7357d329976f095ac7bba
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-01-20 09:32:29 +0100

    Actualiser l'heure dans la balise <p> avec l'heure au format 00-09

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-Number.prototype.padLeft = function(base,chr){
-    var  len = (String(base || 10).length - String(this).length)+1;
-    return len > 0? new Array(len).join(chr || '0')+this : this;
+Number.prototype.padLeft = function (base, chr) {
+    var len = (String(base || 10).length - String(this).length) + 1;
+    return len > 0 ? new Array(len).join(chr || '0') + this : this;
 }
+
